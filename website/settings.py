a65efdf2fdfbcd4d1a7384fea76dd7a1e6c44ec8
ilsur <EarnMarks@yandex.ru>
2021-10-02 16:44:59 +0300

    Added files and changes required for deployment to heroku

--- website/settings.py
+++ website/settings.py
@@ -148,6 +148,3 @@ DATABASES['default'].update(db_from_env)
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
